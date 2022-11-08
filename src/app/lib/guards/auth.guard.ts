54b65aab6a67c5796e40e4204fcce7ae0980bd03
Adan Florez <adan.florezb@gmail.com>
2022-11-08 00:05:43 -0500

    feat: guards

---
+++ src/app/lib/guards/auth.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+      var isAuthenticated = localStorage.getItem("token");
+      if (!isAuthenticated) {
+          this.router.navigate(['/']);
+      }
+      return !!isAuthenticated;
+  }
+}
