e3d96b2dbd3ee6a9b80def69963feae1e9c5ec76
Adan Florez <adan.florezb@gmail.com>
2022-11-07 20:32:03 -0500

    feat: module modal added

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,3 +1,4 @@
+import { ModalModule } from './../shared/modal/modal.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -5,16 +6,14 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
+  declarations: [LoginComponent],
   imports: [
     CommonModule,
     AuthRoutingModule,
     ReactiveFormsModule,
-    FormsModule
-  ]
+    FormsModule,
+    ModalModule,
+  ],
 })
-export class AuthModule { }
+export class AuthModule {}
