dd9a4a0c840381c315a2670721be869d8726bec7
Adan Florez <adan.florezb@gmail.com>
2022-11-09 10:52:06 -0500

    feat: password validate

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,25 +1,41 @@
 import { AlertType } from 'src/app/lib/enums/alert-type';
 import { AuthService } from 'src/app/lib/services/auth.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { PASSWORD_PATERN } from 'src/app/lib/constants';
+
+declare var window: any;
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss'],
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
   showAlert = false;
   alertType: AlertType = AlertType.Danger;
   alertMessage = '';
   signupForm!: FormGroup;
+  passwordFieldType = 'password';
+  passwordButonnIcon = 'lock';
 
   constructor(private authService: AuthService, private router: Router) {
     this.signupForm = new FormGroup({
       name: new FormControl('', Validators.required),
       email: new FormControl('', [Validators.email, Validators.required]),
-      password: new FormControl('', Validators.required),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.pattern(PASSWORD_PATERN),
+      ]),
+    });
+  }
+  ngOnInit(): void {
+    var tooltipTriggerList = [].slice.call(
+      document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    );
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new window.bootstrap.Tooltip(tooltipTriggerEl);
     });
   }
 
@@ -45,5 +61,15 @@ export class SignupComponent {
           this.alertType = AlertType.Danger;
         },
       });
+  }
+
+  showHidePassword(): void {
+    if (this.passwordFieldType === 'password') {
+      this.passwordFieldType = 'text';
+      this.passwordButonnIcon = 'unlock';
+      return;
+    }
+    this.passwordFieldType = 'password';
+    this.passwordButonnIcon = 'lock';
   }
 }
---
+++ src/app/lib/constants/index.ts
@@ -0,0 +1,2 @@
+export const PASSWORD_PATERN =
+  '(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[A-Za-zd$@$!%*?&].{7,}';
