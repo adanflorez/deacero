79b36ce597728f257d2bfe82b8c47d319bd64a9c
Adan Florez <adan.florezb@gmail.com>
2022-11-13 22:58:18 -0500

    feat: donations table component

---
+++ src/app/pages/home/components/donations-table/donations-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-donations-table',
+  templateUrl: './donations-table.component.html',
+  styleUrls: ['./donations-table.component.scss']
+})
+export class DonationsTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -7,12 +7,14 @@ import { HomeComponent } from './home.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AlertModule } from 'src/app/shared/alert/alert.module';
 import { ProductTableComponent } from './components/product-table/product-table.component';
+import { DonationsTableComponent } from './components/donations-table/donations-table.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    ProductTableComponent
+    ProductTableComponent,
+    DonationsTableComponent
   ],
   imports: [
     CommonModule,
