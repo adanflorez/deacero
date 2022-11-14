55293881e1e5e698dd1c8b0fdb7bf1016e6e1cf7
Adan Florez <adan.florezb@gmail.com>
2022-11-13 23:11:03 -0500

    feat: donation table completed

--- src/app/pages/home/components/donations-table/donations-table.component.ts
+++ src/app/pages/home/components/donations-table/donations-table.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ONLY_NUMBERS_PATTERN } from 'src/app/lib/constants';
+import { v4 as uuidv4 } from 'uuid';
 
+interface Donation {
+  id: string;
+  year: string;
+  amount: number;
+  proyectName: string;
+}
 @Component({
   selector: 'app-donations-table',
   templateUrl: './donations-table.component.html',
-  styleUrls: ['./donations-table.component.scss']
+  styleUrls: ['./donations-table.component.scss'],
 })
-export class DonationsTableComponent implements OnInit {
+export class DonationsTableComponent {
+  form: FormGroup;
+  showForm = false;
+  donations: Donation[] = [];
+  validForm = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.form = new FormGroup({
+      year: new FormControl('', [
+        Validators.pattern(ONLY_NUMBERS_PATTERN),
+        Validators.maxLength(4),
+      ]),
+      amount: new FormControl('', Validators.pattern(ONLY_NUMBERS_PATTERN)),
+      proyectName: new FormControl(''),
+    });
+    this.form.valueChanges.subscribe((values) => {
+      const { year, amount, proyectName } = values;
+      if (year || amount || proyectName) {
+        this.validForm = this.form.valid && true;
+        return;
+      }
+      this.validForm = false;
+    });
   }
 
+  showFormOnTable() {
+    this.showForm = true;
+  }
+
+  addDonation() {
+    if (!this.validForm) return;
+    this.showForm = false;
+    this.donations.push({
+      id: uuidv4(),
+      ...this.form.value,
+    });
+    this.form.reset();
+  }
+
+  removeDonation(id: string) {
+    this.donations = this.donations.filter((donation) => donation.id !== id);
+  }
+
+  get f() {
+    return this.form.controls;
+  }
 }
