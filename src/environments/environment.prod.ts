e360fe76823909a1e429e320c4d71e8c980f51f5
Manuel Sebastian Chitiva Rodriguez <mschitiva68@ucatolica.edu.co>
2022-12-08 16:31:45 -0500

    cambio el mensaje de algunas variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,4 +4,6 @@ export const environment = {
   apiUser: 'http://44.228.61.3:8080/user',
   apiRegister: 'http://44.228.61.3:8080/register/',
   apiOSC: 'http://44.228.61.3:8080/osc/',
+  apiMultimedia: 'http://44.228.61.3:8080/multimedia/',
+  apiCall: 'http://44.228.61.3:8080/call/'
 };
