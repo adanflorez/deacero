aa4ebc2b25850c9ece1edac32a2fe0e8644263d8
Adan F <adan.florezb@gmail.com>
2022-12-19 23:34:14 -0500

    feat: objectives form

---
+++ src/app/shared/forms/objectives-form/objectives-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-objectives-form',
+  templateUrl: './objectives-form.component.html',
+  styleUrls: ['./objectives-form.component.scss'],
+})
+export class ObjectivesFormComponent {}
