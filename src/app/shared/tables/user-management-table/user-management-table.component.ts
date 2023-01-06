6c0eaacf4cfd29e19761dad693eaf0715e59641f
Adan F <adan.florezb@gmail.com>
2023-01-06 01:01:34 -0500

    feat: create user form

--- src/app/shared/tables/user-management-table/user-management-table.component.ts
+++ src/app/shared/tables/user-management-table/user-management-table.component.ts
@@ -1,7 +1,12 @@
 import { UserStatusPipe } from 'src/app/lib/pipes/user-status.pipe';
 import { Observable, map, startWith } from 'rxjs';
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CustomValidators } from 'src/app/lib/helpers/custom-validators';
+import { PASSWORD_PATERN } from 'src/app/lib/constants';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+declare let window: any;
 
 interface UserManagement {
   id?: string;
@@ -17,7 +22,7 @@ interface UserManagement {
   styleUrls: ['./user-management-table.component.scss'],
   providers: [UserStatusPipe],
 })
-export class UserManagementTableComponent {
+export class UserManagementTableComponent implements OnInit, AfterViewInit {
   users$: Observable<UserManagement[]>;
   users: UserManagement[];
   filter = new FormControl('', { nonNullable: true });
@@ -25,6 +30,10 @@ export class UserManagementTableComponent {
   activateDeactivateMessage: string;
   createUserForm: FormGroup;
   showCreateUserModal: boolean;
+  passwordFieldType: string;
+  confirmPasswordFieldType: string;
+  passwordButonnIcon: string;
+  confirmPasswordButonnIcon: string;
 
   constructor(private userStatusPipe: UserStatusPipe) {
     this.users$ = this.filter.valueChanges.pipe(
@@ -49,6 +58,47 @@ export class UserManagementTableComponent {
     this.activateDeactivateMessage = '';
     this.createUserForm = new FormGroup({});
     this.showCreateUserModal = false;
+    this.passwordFieldType = 'password';
+    this.confirmPasswordFieldType = 'password';
+    this.passwordButonnIcon = 'lock';
+    this.confirmPasswordButonnIcon = 'lock';
+  }
+  ngAfterViewInit(): void {
+    const tooltipTriggerList = [].slice.call(
+      document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    );
+    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+      return new window.bootstrap.Tooltip(tooltipTriggerEl);
+    });
+  }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  get f() {
+    return this.createUserForm.controls;
+  }
+
+  get passwordMatchError() {
+    return (
+      this.createUserForm.getError('mismatch') &&
+      this.createUserForm.get('confirmPassword')?.touched
+    );
+  }
+
+  initForm() {
+    this.createUserForm = new FormGroup(
+      {
+        user: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [
+          Validators.required,
+          Validators.pattern(PASSWORD_PATERN),
+        ]),
+        confirmPassword: new FormControl('', Validators.required),
+      },
+      [CustomValidators.MatchValidator('password', 'confirmPassword')]
+    );
   }
 
   search(text: string): UserManagement[] {
@@ -79,5 +129,29 @@ export class UserManagementTableComponent {
 
   closeCreateUserModal() {
     this.showCreateUserModal = false;
+  }
+
+  confirmCreateUserModal() {
+    console.log(this.createUserForm.value);
+  }
+
+  showHidePassword(field: number): void {
+    if (field === 1) {
+      if (this.passwordFieldType === 'password') {
+        this.passwordFieldType = 'text';
+        this.passwordButonnIcon = 'unlock';
+        return;
+      }
+      this.passwordFieldType = 'password';
+      this.passwordButonnIcon = 'lock';
+    } else {
+      if (this.confirmPasswordFieldType === 'password') {
+        this.confirmPasswordFieldType = 'text';
+        this.confirmPasswordButonnIcon = 'unlock';
+        return;
+      }
+      this.confirmPasswordFieldType = 'password';
+      this.confirmPasswordButonnIcon = 'lock';
+    }
   }
 }
