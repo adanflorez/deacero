ca8ab8ef736f79b12608c7b42de359d583563760
Adan F <adan.florezb@gmail.com>
2023-01-05 21:52:27 -0500

    feat: user management module

--- src/app/pages/routing.ts
+++ src/app/pages/routing.ts
@@ -1,18 +1,34 @@
 import { Routes } from '@angular/router';
+import { RoleGuard } from 'src/app/lib/guards/role.guard';
 
 const Routing: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
+    data: { role: 'OTHER' },
+    canActivate: [RoleGuard],
   },
   {
     path: 'calls',
     loadChildren: () => import('./calls/calls.module').then(m => m.CallsModule),
+    data: { role: 'OTHER' },
+    canActivate: [RoleGuard],
   },
   {
     path: 'call-alerts',
     loadChildren: () =>
       import('./call-alerts/call-alerts.module').then(m => m.CallAlertsModule),
+    data: { role: 'OTHER' },
+    canActivate: [RoleGuard],
+  },
+  {
+    path: 'user-management',
+    loadChildren: () =>
+      import('./user-management/user-management.module').then(
+        m => m.UserManagementModule
+      ),
+    data: { role: 'ADMINISTRATOR' },
+    canActivate: [RoleGuard],
   },
   {
     path: '',
