fd9d574b6b546add2dda63e2005f97da42f06bbb
Adan F <adan.florezb@gmail.com>
2023-01-06 18:01:23 -0500

    feat: handle error

--- src/app/shared/tables/user-management-table/user-management-table.module.ts
+++ src/app/shared/tables/user-management-table/user-management-table.module.ts
@@ -2,9 +2,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserRolePipe } from 'src/app/lib/pipes/user-role.pipe';
+import { UserStatusPipe } from 'src/app/lib/pipes/user-status.pipe';
 import { AlertModule } from 'src/app/shared/alert/alert.module';
 import { ModalModule } from 'src/app/shared/modal/modal.module';
-import { UserStatusPipe } from 'src/app/lib/pipes/user-status.pipe';
+import { FormErrorModule } from 'src/app/shared/form-error/form-error.module';
 import { UserManagementTableComponent } from './user-management-table.component';
 import { NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -17,6 +18,7 @@ import { NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
     ModalModule,
     ReactiveFormsModule,
     AlertModule,
+    FormErrorModule,
   ],
   exports: [UserManagementTableComponent],
 })
