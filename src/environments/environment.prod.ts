54dc919959ab9da6059bcca829d635a0acdcca66
Adan F <adan.florezb@gmail.com>
2023-01-07 10:37:13 -0500

    feat: feedback endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,6 @@ export const environment = {
   apiRegister: 'http://44.228.61.3:8080/register/',
   apiApplication: 'http://44.228.61.3:8080/application/',
   apiMultimedia: 'http://44.228.61.3:8080/multimedia/',
+  apiFeedback: 'http://44.228.61.3:8080/feedback/',
   apiAdmin: 'http://44.228.61.3:8080/admin/',
 };
