7030beca3731a7ca00145dd97a749861bc7744db
Adan F <adan.florezb@gmail.com>
2023-01-15 18:50:36 -0500

    feat: strategic-alliances-activities-table component

---
+++ src/app/shared/tables/strategic-alliances-activities-table/strategic-alliances-activities-table.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-strategic-alliances-activities-table',
+  templateUrl: './strategic-alliances-activities-table.component.html',
+  styleUrls: ['./strategic-alliances-activities-table.component.scss'],
+})
+export class StrategicAlliancesActivitiesTableComponent {}
---
+++ src/app/shared/tables/strategic-alliances-activities-table/strategic-alliances-activities-table.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StrategicAlliancesActivitiesTableComponent } from './strategic-alliances-activities-table.component';
+
+@NgModule({
+  declarations: [StrategicAlliancesActivitiesTableComponent],
+  imports: [CommonModule],
+  exports: [StrategicAlliancesActivitiesTableComponent],
+})
+export class StrategicAlliancesActivitiesTableModule {}
