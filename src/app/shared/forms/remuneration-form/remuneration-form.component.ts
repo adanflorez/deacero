5c17ad97444f73c9e82da1b0a68b815c7ebd5c48
Adan F <adan.florezb@gmail.com>
2023-01-24 14:59:47 -0500

    fix: errors

--- src/app/shared/forms/remuneration-form/remuneration-form.component.ts
+++ src/app/shared/forms/remuneration-form/remuneration-form.component.ts
@@ -48,7 +48,7 @@ export class RemunerationFormComponent implements OnInit, OnDestroy, OnChanges {
 
   ngOnChanges(changes: SimpleChanges): void {
     const { disable } = changes;
-    if (disable.currentValue) {
+    if (disable?.currentValue) {
       this.form.disable();
     }
   }
