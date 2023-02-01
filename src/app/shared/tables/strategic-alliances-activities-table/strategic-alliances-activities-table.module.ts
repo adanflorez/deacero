a8549ff4c719090d10dc7c3c28adac3b59695720
Adan F <adan.florezb@gmail.com>
2023-01-31 22:21:02 -0500

    feat: show alert

--- src/app/shared/tables/strategic-alliances-activities-table/strategic-alliances-activities-table.module.ts
+++ src/app/shared/tables/strategic-alliances-activities-table/strategic-alliances-activities-table.module.ts
@@ -1,11 +1,13 @@
-import { ReactiveFormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { StrategicAlliancesActivitiesTableComponent } from './strategic-alliances-activities-table.component';
 
 @NgModule({
   declarations: [StrategicAlliancesActivitiesTableComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, NgbAlertModule],
   exports: [StrategicAlliancesActivitiesTableComponent],
 })
 export class StrategicAlliancesActivitiesTableModule {}
