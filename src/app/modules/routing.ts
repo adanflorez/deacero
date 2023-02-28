c9c288a20391245362adcc845a4adcf55d031b89
Sebastian R <38385752+ChiVeto96@users.noreply.github.com>
2023-02-27 22:34:10 -0500

    Update routing.ts

--- src/app/modules/routing.ts
+++ src/app/modules/routing.ts
@@ -5,20 +5,20 @@ const Routing: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
-    data: { role: 'CLIENT' },
+    data: { role: 'OSC' },
     canActivate: [RoleGuard],
   },
   {
     path: 'calls',
     loadChildren: () => import('./calls/calls.module').then(m => m.CallsModule),
-    data: { role: 'CLIENT' },
+    data: { role: 'OSC' },
     canActivate: [RoleGuard],
   },
   {
     path: 'call-alerts',
     loadChildren: () =>
       import('./call-alerts/call-alerts.module').then(m => m.CallAlertsModule),
-    data: { role: 'CLIENT' },
+    data: { role: 'OSC' },
     canActivate: [RoleGuard],
   },
   {
