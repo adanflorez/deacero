9d2321f30940965496ccdd585095b863479dcaa8
Adan F <adan.florezb@gmail.com>
2023-04-25 21:15:01 -0500

    feat: redirect to home if administrator

--- src/app/core/guards/announcement.guard.ts
+++ src/app/core/guards/announcement.guard.ts
@@ -1,21 +1,29 @@
 import { Injectable, Injector } from '@angular/core';
 import { CanActivate, Router, UrlTree } from '@angular/router';
-import { Observable, firstValueFrom } from 'rxjs';
+import { firstValueFrom } from 'rxjs';
+
 import { AuthService } from '../services/auth.service';
+import { RoleService } from '../services/role.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AnnouncementGuard implements CanActivate {
   private authService: AuthService;
+  private roleService: RoleService;
   private router: Router;
 
   constructor(private readonly injector: Injector) {
     this.authService = this.injector.get(AuthService);
+    this.roleService = this.injector.get(RoleService);
     this.router = this.injector.get(Router);
   }
   async canActivate(): Promise<boolean | UrlTree> {
     try {
+      const role = await firstValueFrom(this.roleService.role());
+      if (role === 'ADMINISTRATOR') {
+        return true;
+      }
       const { data } = await firstValueFrom(
         this.authService.announcementIsAvailable()
       );
