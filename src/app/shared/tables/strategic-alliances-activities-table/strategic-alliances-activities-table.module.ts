816b846c663cd0e826b0bbd0b6f518f5208796fe
Adan Florez <aflorez@rapicredit.com>
2023-05-05 10:44:44 -0500

    refactor: form error module

--- src/app/shared/tables/strategic-alliances-activities-table/strategic-alliances-activities-table.module.ts
+++ src/app/shared/tables/strategic-alliances-activities-table/strategic-alliances-activities-table.module.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormErrorModule } from 'src/app/shared/form-error/form-error.module';
 
 import { StrategicAlliancesActivitiesTableComponent } from './strategic-alliances-activities-table.component';
 
 @NgModule({
   declarations: [StrategicAlliancesActivitiesTableComponent],
-  imports: [CommonModule, ReactiveFormsModule, NgbAlertModule],
+  imports: [CommonModule, ReactiveFormsModule, NgbAlertModule, FormErrorModule],
   exports: [StrategicAlliancesActivitiesTableComponent],
 })
 export class StrategicAlliancesActivitiesTableModule {}
